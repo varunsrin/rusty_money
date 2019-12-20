0e2194ef5bcdda6dd400753db3d62a7d9171742e
Varun Srinivasan <varunsrin@gmail.com>
2019-12-20 23:54:36 +0530

    Update: Fixing docs

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,7 @@
 //! ```edition2018
 //! use rusty_money::money;
 //! use rusty_money::Money;
-//! use rusty_money::Currency;
-//! 
+//!
 //! money = money!("-200.00", "USD");
 //! money = money!(-200, "USD");
 //! ```
@@ -19,8 +18,11 @@
 //! Money handles rounding and formatting for you based on the properties of the currency:    
 //!
 //! ```edition2018
+//! //! use rusty_money::money;
+//! use rusty_money::Money;
+//! 
 //! money = money!("-2000.009", "USD");
-//! println!("{}", money) // -$2,000.01;
+//! println!("{}", money); // -$2,000.01
 //!
 //! money = money!("-2000.009", "EUR");
 //! println!("{}", money) // -€2,000.01;
@@ -29,6 +31,9 @@
 //! You can perform basic operations on money like:
 //!
 //! ```edition2018
+//! //! use rusty_money::money;
+//! use rusty_money::Money;
+//! 
 //! hundred = money!("100", "USD");
 //! thousand = money!("1000", "USD");
 //! println!("{}", hundred + thousand);     // $1,000.00 USD
