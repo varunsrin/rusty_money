3276c7d9a2767d945d1cbff7c90b65c35570e6ab
Varun Srinivasan <varunsrin@gmail.com>
2019-12-24 15:24:26 +0530

    Feature: Lookup currency by ISO numeric code

--- src/currency.rs
+++ src/currency.rs
@@ -7,7 +7,10 @@ use std::str::FromStr;
 static CURRENCY_JSON: &str = include_str!("../config/currencies.json");
 
 lazy_static! {
-    static ref CURRENCIES: HashMap<String, Currency> = serde_json::from_str(CURRENCY_JSON).unwrap();
+    static ref CURRENCIES_BY_ALPHA_CODE: HashMap<String, Currency> =
+        serde_json::from_str(CURRENCY_JSON).unwrap();
+    static ref CURRENCIES_BY_NUM_CODE: HashMap<String, Currency> =
+        Currency::generate_currencies_by_num_code();
 }
 
 /// The `Currency` type, which stores metadata about an ISO-4127 currency.
@@ -30,11 +33,30 @@ impl fmt::Display for Currency {
 }
 
 impl Currency {
-    /// Finds a currency type given an alphabetic ISO-4217 currency code.
-    pub fn find(name: String) -> Currency {
-        match CURRENCIES.get(&name.to_lowercase()) {
+    /// Finds a currency type given an ISO-4217 currency code.
+    pub fn find(code: String) -> Currency {
+        if code.chars().all(char::is_alphabetic) {
+            Currency::find_by_alpha_iso(code)
+        } else if code.chars().all(char::is_numeric) {
+            Currency::find_by_numeric_iso(code)
+        } else {
+            panic!("{} is not a known currency", code);
+        }
+    }
+
+    /// Finds a currency type given an alphabetic currency code.
+    pub fn find_by_alpha_iso(code: String) -> Currency {
+        match CURRENCIES_BY_ALPHA_CODE.get(&code.to_lowercase()) {
             Some(c) => *c,
-            None => panic!("{} is not a known currency", name), //TODO - more helpful message
+            None => panic!("{} is not a known currency", code), //TODO - more helpful message
+        }
+    }
+
+    /// Finds a currency type given a numeric ISO-4217 currency code.
+    pub fn find_by_numeric_iso(code: String) -> Currency {
+        match CURRENCIES_BY_NUM_CODE.get(&code.to_lowercase()) {
+            Some(c) => *c,
+            None => panic!("{} is not a known currency", code), //TODO - more helpful message
         }
     }
 
@@ -43,6 +65,15 @@ impl Currency {
         let v: Vec<&str> = self.digit_separator_sequence.split(", ").collect();
         v.iter().map(|x| usize::from_str(x).unwrap()).collect()
     }
+
+    /// Returns a hashmap of currencies indexed by their ISO numeric code.
+    fn generate_currencies_by_num_code() -> HashMap<String, Currency> {
+        let mut num_map: HashMap<String, Currency> = HashMap::new();
+        for (_k, v) in CURRENCIES_BY_ALPHA_CODE.iter() {
+            num_map.insert(v.iso_numeric_code.to_string(), *v);
+        }
+        num_map
+    }
 }
 
 #[cfg(test)]
@@ -50,14 +81,24 @@ mod tests {
     use super::*;
     #[test]
     fn currency_known_can_be_found() {
-        let c = Currency::find("USD".to_string());
-        assert_eq!(c.iso_alpha_code, "USD");
-        assert_eq!(c.exponent, 2);
+        let currency_by_alpha = Currency::find("USD".to_string());
+        assert_eq!(currency_by_alpha.iso_alpha_code, "USD");
+        assert_eq!(currency_by_alpha.exponent, 2);
+        assert_eq!(currency_by_alpha.symbol, "$");
+
+        let currency_by_numeric = Currency::find("840".to_string());
+        assert_eq!(currency_by_alpha, currency_by_numeric);
     }
 
     #[test]
     #[should_panic]
-    fn currency_unknown_raises_error() {
+    fn currency_unknown_iso_alpha_code_raises_error() {
         Currency::find("fake".to_string());
+    }
+
+    #[test]
+    #[should_panic]
+    fn currency_unknown_iso_num_code_raises_error() {
+        Currency::find("123".to_string());
     }
 }
