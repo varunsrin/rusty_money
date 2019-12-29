2cdffecd2d77e1de05469683758c5a8fa3581adb
Varun Srinivasan <varunsrin@gmail.com>
2019-12-29 11:06:51 +0530

    Feature: Get currencies using enums for safer calls

---
+++ src/currency/iso.rs
@@ -0,0 +1,98 @@
+use crate::currency::Currency;
+use std::fmt;
+
+// Allows iterating over the Iso Enum
+macro_rules! define_enum {
+    ($Name:ident { $($Variant:ident),* $(,)* }) =>
+    {
+        #[derive(Debug)]
+        pub enum $Name {
+            $($Variant),*,
+        }
+        pub const ISO_CURRENCIES: &'static [$Name] = &[$($Name::$Variant),*];
+    }
+}
+
+// Enum that represents every ISO Currency
+define_enum!(Iso {
+    AED,
+    BHD,
+    EUR,
+    GBP,
+    INR,
+    USD,
+});
+
+impl fmt::Display for Iso {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// Returns Currency given an Iso Enum.
+pub fn from_enum(code: &Iso) -> Currency {
+    use Iso::*;
+
+    match code {
+        AED => Currency {
+            digit_separator: ',',
+            digit_separator_sequence: "3, 3, 3",
+            exponent: 2,
+            exponent_separator: '.',
+            iso_alpha_code: "AED",
+            iso_numeric_code: "784",
+            symbol: "د.إ",
+            symbol_first: false,
+        },
+        BHD => Currency {
+            digit_separator: ',',
+            digit_separator_sequence: "3, 3, 3",
+            exponent: 3,
+            exponent_separator: '.',
+            iso_alpha_code: "BHD",
+            iso_numeric_code: "048",
+            symbol: "ب.د",
+            symbol_first: true,
+        },
+        EUR => Currency {
+            digit_separator: '.',
+            digit_separator_sequence: "3, 3, 3",
+            exponent: 2,
+            exponent_separator: ',',
+            iso_alpha_code: "EUR",
+            iso_numeric_code: "978",
+            symbol: "€",
+            symbol_first: true,
+        },
+        GBP => Currency {
+            digit_separator: ',',
+            digit_separator_sequence: "3, 3, 3",
+            exponent: 2,
+            exponent_separator: '.',
+            iso_alpha_code: "GBP",
+            iso_numeric_code: "826",
+            symbol: "£",
+            symbol_first: true,
+        },
+        INR => Currency {
+            digit_separator: ',',
+            digit_separator_sequence: "3, 2, 2",
+            exponent: 2,
+            exponent_separator: '.',
+            iso_alpha_code: "INR",
+            iso_numeric_code: "356",
+            symbol: "₹",
+            symbol_first: true,
+        },
+        USD => Currency {
+            digit_separator: ',',
+            digit_separator_sequence: "3, 3, 3",
+            exponent: 2,
+            exponent_separator: '.',
+            iso_alpha_code: "USD",
+            iso_numeric_code: "840",
+            symbol: "$",
+            symbol_first: true,
+        },
+    }
+}
